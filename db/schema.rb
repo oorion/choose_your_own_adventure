9087b0280ac537a3ee9be0350c82d7a9e9cc0bf6
Orion Osborn <oosborn@invoca.com>
2016-07-31 10:56:29 -0700

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731173004) do
+ActiveRecord::Schema.define(version: 20160731175117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "stories", force: :cascade do |t|
-    t.string "text"
-    t.string "title"
+    t.string   "text"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
